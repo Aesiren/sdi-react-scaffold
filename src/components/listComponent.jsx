b903cf3e23d8a8cc39f55d1e747aaf24b15cbf99
Evan M. Bonk <embonk@hotmail.com>
2025-07-25 15:01:13 -0600

    started adjusting list component logic

--- src/components/listComponent.jsx
+++ src/components/listComponent.jsx
@@ -2,6 +2,32 @@ import ItemComponent from "./itemComponent.jsx";
 
 
 function ListComponent() {
+  const [items, setItems] = useState("Null");
+
+
+  //useEffect to fill an array of item
+  //for each item, create an item component with a listed item
+  //each item component has a selected value
+  //upon selecting an item, pass all data to the ViewComponent
+
+
+  async function getData() {
+    let length = dummyData.drinks.length;
+    //console.log("length: ", length);
+    let random = Math.floor(Math.random() * length)
+    console.log("Random: ", random);
+    let idName = dummyData.drinks[random]["idDrink"];
+    console.log("idName: ", idName);
+    let data = await Fetch(idName, "id");
+    console.log("Data submitted to drinkClass: ", data);
+    //let drink = new drinkClass(data);
+    return data;
+    // setTimeout(() => {
+    //   console.log("Data submitted to drinkClass: ", data);
+    //   let drink = new drinkClass(data);
+    //   setItem(drink);
+    // }, 3000);
+  }
 
   return (
     <>
